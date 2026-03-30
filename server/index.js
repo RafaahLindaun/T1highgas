96a42b2c3102ad7107ad8b16bb69464dedd58b44
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-03-30 05:57:32 -0300

    Initialize Express server with maps router

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import mapsRouter from "./routes/maps.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/maps", mapsRouter);
+
+app.listen(3001, () => {
+  console.log("Server rodando na porta 3001");
+});
