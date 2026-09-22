a458b72b5df44c638697109b565d92b2e1fa2459
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-22 05:05:52 -0300

    fix: bundle JARVIS styles in production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import JarvisPanel from "./components/JarvisPanel";
+import "./jarvis.css";
 import {
   fallbackServers,
   loadVpnServers,
